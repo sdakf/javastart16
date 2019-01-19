b1f31ed2a63fc7e9e5f9778f4148a09e6610c67d
Krzysztof Faliński <k.falinski@sdacademy.pl>
2019-01-19 17:06:42 +0100

    lombok

---
+++ src/main/java/pl/sda/javastart/day7/SuperLongClass.java
@@ -0,0 +1,18 @@
+package pl.sda.javastart.day7;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Getter
+@Setter
+
+public class SuperLongClass {
+
+    private int someValue;
+    private String anotherValue;
+    private String test;
+    private String value;
+    private BigDecimal price;
+}
